3c51d3c6e6ec97131f7e4dd3e21e53c4068dca69
chris-tinah <71039185+chris-tinah@users.noreply.github.com>
2021-09-11 17:38:58 -0700

    created food constructor and declared more fields related to max and min int

--- src/main/model/food.java
+++ src/main/model/food.java
@@ -2,9 +2,34 @@ package model;
 
 // Represents food with expiry time, nutritional value, and coordinates in x and y direction
 public class food {
-    private int expiry;
-    private int value;
-    private int xCoor;
-    private int yCoor;
+    private int expiry = 10;
 
+    private int value;
+    private int maxVal = 3;
+    private int minVal = 1;
+
+    private int xCoor;
+    private int minX = 0;
+    private int maxX = 0;
+
+    private int yCoor;
+    private int minY = 0;
+    private int maxY = 0;
+
+
+    // Codes to generate random int references https://www.educative.io/edpresso/how-to-generate-random-numbers-in-java
+    // EFFECTS: creates a new food with default expiry, random value, and random x and y coordinates
+    public food() {
+        int rangeVal = (maxVal - minVal + 1);
+        int rangeX = (maxX - minX + 1);
+        int rangeY = (maxY - minY + 1);
+
+        int randomVal = (int)Math.floor(Math.random() * rangeVal + minVal);
+        int randomX = (int)Math.floor(Math.random() * rangeX + minX);
+        int randomY = (int)Math.floor(Math.random() * rangeY + minY);
+
+        value = randomVal;
+        xCoor = randomX;
+        yCoor = randomY;
+    }
 }
