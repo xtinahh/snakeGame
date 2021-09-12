7f6f10f4178f1fb47e7da5ae18c3b0bfdf7d4596
chris-tinah <71039185+chris-tinah@users.noreply.github.com>
2021-09-12 14:38:33 -0700

    modified fields in snake class related to how to represent a snake and created getter methods for those fields

--- src/main/model/Snake.java
+++ src/main/model/Snake.java
@@ -3,64 +3,59 @@ package model;
 import java.util.ArrayList;
 import java.util.List;
 
-// Represent a snake with its length increasing as it consumes food moving at a default speed
+// Represent a snake with its length increasing as it consumes food
 public class Snake extends GameObject {
-    private int x_Pos;
-    protected int y_Pos;
-    private int SPEED;
-    private Body head;
-    private List<Body> length;
+    private Section head;
+    private List<Section> body;
+    private String direction;
 
-    // EFFECTS: creates a snake with one length in the middle of the stage
+    // EFFECTS: creates a snake with a head
     public Snake() {
-        length = new ArrayList<Body>();
-        length.add(new Body());
-        x_Pos = MAX_X / 2;
-        y_Pos = MAX_Y / 2;
+        body = new ArrayList<Section>();
+        body.add(new Section());
     }
 
+    // EFFECTS: returns the length of snake (head and body)
     public int getLength() {
-        return length.size();
+        return body.size();
     }
 
-    public int getX_Pos() {
-        return x_Pos;
-    }
-
-    public int getY_Pos() {
-        return y_Pos;
+    // EFFECTS: returns direction of snake
+    public String getDirection() {
+        return direction;
     }
 
     // MODIFIES: this
-    // EFFECTS: increases the length of the snake by one
+    // EFFECTS: increases the length of tail by one
     public void grow() {
-        length.add(new Body());
+        body.add(new Section());
     }
 
-    // MODIFIES: this
-    // EFFECTS: changes direction of snake 90 degrees to the right
-    public void rotateRight() {
-
-    }
-
-    // MODIFIES: this
-    // EFFECTS: changes direction of snake 90 degrees to the left
-    public void rotateLeft() {
-
-    }
-
-//    // MODIFIES: this
+    //    // MODIFIES: this
 //    // EFFECTS: reduces the length of the snake by one
 //    public void shrink() {
 //        // consider scenarios where there is only one length left
 //        length.remove(0);
 //    }
 
+    // MODIFIES: this
+    // EFFECTS: rotates snake 90 degrees to the right
+    public void rotateRight() {
+        // TODO
+    }
+
+    // MODIFIES: this
+    // EFFECTS: rotates snake 90 degrees to the left
+    public void rotateLeft() {
+        // TODO
+    }
+
     // EFFECTS: produces true if snake crossed the boundary
     public Boolean crossedBoundary() {
         // TODO: consider scenarios where snake is moving along the boundary
-        return ((x_Pos == MIN_X) || (x_Pos == MAX_X) ||
-                (y_Pos == MIN_Y) || (y_Pos == MAX_Y));
+//        return ((x_Pos == MIN_X) || (x_Pos == MAX_X) ||
+//                (y_Pos == MIN_Y) || (y_Pos == MAX_Y));
+        return false;
     }
 
 
