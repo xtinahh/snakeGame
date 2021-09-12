10e8cacdc23cad625758a8fa8b85c686dc3234e3
chris-tinah <71039185+chris-tinah@users.noreply.github.com>
2021-09-11 18:43:36 -0700

    described snake class and created its fields and constructor

--- src/main/model/snake.java
+++ src/main/model/snake.java
@@ -1,5 +1,15 @@
 package model;
 
-public class snake extends gameObject{
+import java.util.ArrayList;
 
+// Represent a snake with its length increasing as it consumes food
+public class snake extends gameObject{
+    private int SPEED;
+    private List<body> length;
+
+    // EFFECTS: creates a snake with one length
+    public snake() {
+        length = new ArrayList<body>();
+        length.addBody();
+    }
 }
