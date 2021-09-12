ab211499be4ddd6586bb4068ac8d7169d370d30b
chris-tinah <71039185+chris-tinah@users.noreply.github.com>
2021-09-12 15:33:02 -0700

    modified fields in stage class and added constants for size of stage

--- src/main/model/Stage.java
+++ src/main/model/Stage.java
@@ -1,17 +1,17 @@
 package model;
 
-import java.util.ArrayList;
-import java.util.List;
-
-// Represents the stage where game events take place
+// Represents a stage with size STAGE_COLS * STAGE_ROWS
 public class Stage {
+    public static final int STAGE_COLS = 12;
+    public static final int STAGE_ROWS = 12;
+
     private Snake snake;
-    private List<Food> foodList;
+    private Food food;
 
     // EFFECTS: creates a new stage with a snake and empty foodList
     public Stage() {
-        snake = new Snake();
-        foodList = new ArrayList<>();
+        snake = new Snake( new Section(STAGE_COLS/2, STAGE_ROWS/2));
+        food = new Food();
     }
 
 }
