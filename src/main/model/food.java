f652fef6683c5d9dfbfaf3f5de775260398b3823
chris-tinah <71039185+chris-tinah@users.noreply.github.com>
2021-09-11 17:10:28 -0700

    added description of food class

--- src/main/model/food.java
+++ src/main/model/food.java
@@ -1,8 +1,10 @@
 package model;
 
+// Represents food with expiry time, nutritional value, and coordinates in x and y direction
 public class food {
     private int expiry;
     private int value;
     private int xCoor;
     private int yCoor;
+
 }
