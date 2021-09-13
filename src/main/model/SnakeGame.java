f5633baeb077679552e5972ec20b22801e6ee972
chris-tinah <71039185+chris-tinah@users.noreply.github.com>
2021-09-12 18:30:26 -0700

    created SnakeGame constructor and a helper method

--- src/main/model/SnakeGame.java
+++ src/main/model/SnakeGame.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.util.Random;
+
 // Represents a snake game with its stage size STAGE_COLS * STAGE_ROWS
 public class SnakeGame {
     public static final int STAGE_COLS = 12;
@@ -12,12 +14,14 @@ public class SnakeGame {
     // EFFECTS: creates a new snake game with a snake in the middle of the stage and food in a random location
     public SnakeGame() {
         snake = new Snake(new Section(STAGE_COLS/2, STAGE_ROWS/2));
-        // TODO: created food in random place
+        food = createFood();
     }
 
-//    // EFFECTS: returns food in a random location other than where the snake is
-//    public Food createFood() {
-//        // TODO
-//    }
+    // EFFECTS: returns food in a random location other than where the snake is
+    public Food createFood() {
+        Random randomNumb = new Random();
+        Section randomSection = new Section (randomNumb.nextInt(STAGE_COLS), randomNumb.nextInt(STAGE_ROWS));
+        return new Food(randomSection);
+    }
 
 }
