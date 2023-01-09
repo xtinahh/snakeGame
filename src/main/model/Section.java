aa32c2853735b486c935f9e6175a09506aec824b
Christina Han <71039185+xtinahh@users.noreply.github.com>
2023-01-08 21:27:52 -0800

    add more clarity to Section.java function description

--- src/main/model/Section.java
+++ src/main/model/Section.java
@@ -2,25 +2,25 @@ package model;
 
 import java.util.Objects;
 
-// Represents a section in the game board
+// Represents a section of the snake's body in the game board
 public class Section {
     public static final int SECTION_SIZE = 40;
     private int column;
     private int row;
 
 
-    // EFFECTS: creates a section at the given position
+    // EFFECTS: creates a section of the snake's body at the given position
     public Section(int col, int row) {
         this.column = col;
         this.row = row;
     }
 
-    // EFFECTS: returns column of this section
+    // EFFECTS: returns the x-position of this section
     public int getColumn() {
         return column;
     }
 
-    // EFFECTS: returns row of this section
+    // EFFECTS: returns the y-position of this section
     public int getRow() {
         return row;
     }
